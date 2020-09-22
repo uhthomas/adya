2fec862216535214330cbfe82fa135fb4ec53ec4
Thomas <tway@cloudflare.com>
2020-09-22 01:23:31 +0100

    back-off connect

--- cmd/adya/main.go
+++ cmd/adya/main.go
@@ -6,11 +6,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/uhthomas/adya/internal"
 )
 
-func Main(context.Context) error {
+func sleep(ctx context.Context, d time.Duration) {
+	t := time.NewTimer(d)
+	defer t.Stop()
+
+	select {
+	case <-t.C:
+	case <-ctx.Done():
+	}
+}
+
+func Main(ctx context.Context) error {
 	token, ok := os.LookupEnv("TOKEN")
 	if !ok {
 		return errors.New("missing token")
@@ -20,12 +32,22 @@ func Main(context.Context) error {
 	if err != nil {
 		return fmt.Errorf("new session: %w", err)
 	}
-	defer s.Close()
 
-	if err := s.Open(); err != nil {
-		return fmt.Errorf("open: %w", err)
+	s.AddHandler(internal.Handle)
+
+	for i := time.Duration(0); ; i++ {
+		if err := s.Open(); err != nil {
+			if i <= 5 {
+				sleep(ctx, i*2*time.Second)
+				continue
+			}
+			return fmt.Errorf("open: %w", err)
+		}
+		defer s.Close()
+		break
 	}
 
+	<-ctx.Done()
 	return nil
 }
 
