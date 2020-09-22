27889329e7ee775c4afe4b12e9a55119579c1b7e
Thomas <tway@cloudflare.com>
2020-09-22 02:15:26 +0100

    Restrict intents, remove egress debug code

--- cmd/adya/main.go
+++ cmd/adya/main.go
@@ -5,37 +5,13 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/uhthomas/adya/internal"
 )
 
-func egress(ctx context.Context, c *http.Client) error {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://google.com/", nil)
-	if err != nil {
-		return fmt.Errorf("new request: %w", err)
-	}
-	res, err := c.Do(req)
-	if err != nil {
-		return fmt.Errorf("do request: %w", err)
-	}
-	defer res.Body.Close()
-	return nil
-}
-
 func Main(ctx context.Context) error {
-	c := &http.Client{Timeout: 5 * time.Second}
-	for i := 0; i < 5; i++ {
-		if err := egress(ctx, c); err != nil {
-			log.Println(err)
-		} else {
-			log.Println("ok!")
-		}
-	}
-
 	token, ok := os.LookupEnv("TOKEN")
 	if !ok {
 		return errors.New("missing token")
@@ -48,7 +24,7 @@ func Main(ctx context.Context) error {
 
 	s.AddHandler(internal.Handle)
 
-	s.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAll)
+	s.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
 
 	if err := s.Open(); err != nil {
 		return fmt.Errorf("open: %w", err)
